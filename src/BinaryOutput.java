6e2685af00be134e70dfec20e796d47fa19858e6
arianaazarbal <arianaazarbal@icloud.com>
2021-09-08 15:47:58 -0700

    binary updates

--- src/BinaryOutput.java
+++ src/BinaryOutput.java
@@ -1,11 +1,20 @@
 
 public class BinaryOutput {
 	private int decimal;
+	private int binary;
 	
 	
 	public BinaryOutput(int decimal)
 	{
-		
+		binary = 0;
+		this.decimal = decimal;
+		while (decimal != 0)
+		{
+			int remainder = decimal%2;
+			binary = binary* 10 + remainder;
+			decimal = decimal/2;
+		}
 	}
 
+	
 }

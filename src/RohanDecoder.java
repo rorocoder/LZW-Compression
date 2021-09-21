ddb1f2a6fd0531930204f73eba73db7089fdcd9f
donggunkwak <47698773+donggunkwak@users.noreply.github.com>
2021-09-21 12:17:19 -0700

    Implemented Time Measurement, Error/Exception Checking, and Cut down time
    Time differences and exceptions/errors shown in ReadMe

--- src/RohanDecoder.java
+++ src/RohanDecoder.java
@@ -4,112 +4,131 @@ import java.io.*;
 import java.nio.file.*;
 
 public class RohanDecoder {
-	
+
 	public HashMap<Integer, String> dictionary;
-	public String binary;
+	public StringBuilder binary;
 	int binaryIndex;
 	ArrayList<Integer> numberValues;
-	
+
 	public RohanDecoder() {
-		binary = "";
+		binary = new StringBuilder();
 		dictionary = new HashMap<Integer, String>();
 		this.makeDictionary();
 	}
-	
+
 	public void makeDictionary() { 
 		for(int i = 0; i<256; i++) {
 			dictionary.put(i, "" + (char)i);
 		}
 	}
-	
-	public void ByteToBinary() throws IOException {
-		 FileInputStream input = new FileInputStream("trashbin");
-	    
-	     binary = ""; 
-	     byte[] byteArray = input.readAllBytes();
-	     //System.out.println(byteArray);
-	     
-	     for(int i =0;i<byteArray.length;i++)
-			{
-				//System.out.println(byteArray[i]+" "+toBinary(byteArray[i]));
 
-				binary+= this.toBinary(byteArray[i]);
-				//System.out.println(binary);
-			}
-	     input.close();
-		
-	}
-	
-	public void decode() throws IOException { //algorithm from https://www.geeksforgeeks.org/lzw-lempel-ziv-welch-compression-technique/
-		this.ByteToBinary();
-		PrintWriter outputWriter = new PrintWriter(new BufferedWriter(new FileWriter("decoded.txt")));
-		
-		binaryIndex = 0;
-		
-		this.makeNumbersFromBinary(); //fills the variable numberValues with the number representations of each 9 bit binary string
-		//System.out.println(this.numberValues);
-		
-		Integer old = numberValues.get(0);
-		outputWriter.print(dictionary.get(old)); //outputs the value from old
-		
-		int nextDictIndex = 256;
-		String combination = dictionary.get(old);
-		String firstLetter = "" + combination.charAt(0);
-		
-		
-		for(int i = 1; i< numberValues.size(); i+=1) {
-			Integer newest = numberValues.get(i);
-			
-			if(dictionary.containsKey(newest)) {
-				combination = dictionary.get(newest); //if it contains newest then we set the combo to its value
-			}
-			else {
-				combination = dictionary.get(old); //if it doenst contain newest, then we set combo to old + firstLetter
-				combination = combination + firstLetter;
-			}
-			
-			outputWriter.print(combination);
-			
-			firstLetter = "" + combination.charAt(0);
-			dictionary.put(nextDictIndex, dictionary.get(old) + combination);
-			nextDictIndex+=1;
-			old = newest;
+	public void ByteToBinary() throws IOException {
+		FileInputStream input = new FileInputStream("encoded.txt");
+
+		binary = new StringBuilder(); 
+		byte[] byteArray = input.readAllBytes();
+		//System.out.println(byteArray);
+
+		for(int i =0;i<byteArray.length;i++)
+		{
+			//System.out.println(byteArray[i]+" "+toBinary(byteArray[i]));
+
+			binary.append(this.toBinary(byteArray[i]));
+			//System.out.println(binary);
 		}
-		
-		outputWriter.close();
-//		System.out.println(old);
-//		System.out.println(dictionary.get(old));
-		
-		
-//		String Binary2 = binary.substring(9,18);
-//		System.out.println("binary " + oldBinary);
-//		System.out.println("binary " + Binary2);
+		input.close();
+
 	}
-	
-	
+
+	public void decode() throws IOException { //algorithm from https://www.geeksforgeeks.org/lzw-lempel-ziv-welch-compression-technique/
+		try
+		{
+			this.ByteToBinary();
+			PrintWriter outputWriter = new PrintWriter(new BufferedWriter(new FileWriter("decoded.txt")));
+
+			binaryIndex = 0;
+
+			this.makeNumbersFromBinary(); //fills the variable numberValues with the number representations of each 9 bit binary string
+			//System.out.println(this.numberValues);
+
+			Integer old = numberValues.get(0);
+			outputWriter.print(dictionary.get(old)); //outputs the value from old
+
+			int nextDictIndex = 256;
+			try
+			{
+				String combination = dictionary.get(old);
+				String firstLetter = "" + combination.charAt(0);
+			}
+			catch(Exception E)
+			{
+				System.out.println(E);
+				System.out.println("Error: Cannot find \"numberValues.get(0)/first value of old\" in the dictionary");
+				return;
+			}
+			String combination = dictionary.get(old);
+			String firstLetter = "" + combination.charAt(0);
+
+
+			for(int i = 1; i< numberValues.size(); i++) {
+				int newest = numberValues.get(i);
+
+				if(dictionary.containsKey(newest)) {
+					combination = dictionary.get(newest); //if it contains newest then we set the combo to its value
+				}
+				else {
+					combination = dictionary.get(old); //if it doenst contain newest, then we set combo to old + firstLetter
+					combination = new StringBuilder(combination).append(firstLetter).toString();
+				}
+
+				outputWriter.print(combination);
+
+				firstLetter = "" + combination.charAt(0);
+				dictionary.put(nextDictIndex, dictionary.get(old) + combination);
+				nextDictIndex+=1;
+				old = newest;
+			}
+
+			outputWriter.close();
+			//		System.out.println(old);
+			//		System.out.println(dictionary.get(old));
+
+
+			//		String Binary2 = binary.substring(9,18);
+			//		System.out.println("binary " + oldBinary);
+			//		System.out.println("binary " + Binary2);
+		}
+		catch(Exception E)
+		{
+			System.out.println(E);
+			System.out.println("Error: Decoder did not work for a reason other than the first value not being found in the dictionary");
+		}
+	}
+
+
 	public Integer getNextNumberFromBinary() {
-	
+
 		String nextBinary = this.binary.substring(this.binaryIndex, this.binaryIndex+9);
 		binaryIndex+=9;
 		return Integer.parseInt(nextBinary,2);
 	}
-	
+
 	public void makeNumbersFromBinary() {
 		this.numberValues = new ArrayList<Integer>();
-		
+
 		for(int i = 0;i <= binary.length() - 9; i+=9) {
-//			System.out.println("next number = " + this.getNextNumberFromBinary());
-//			System.out.println("list =  " + this.numberValues);
+			//			System.out.println("next number = " + this.getNextNumberFromBinary());
+			//			System.out.println("list =  " + this.numberValues);
 			//System.out.println(dictionary.get(this.getNextNumberFromBinary()));
 			//numberValues.add(getNextNumberFromBinary());
-//			if(i+9 > binary.length()) {
-//				break;
-//			}
-			
+			//			if(i+9 > binary.length()) {
+			//				break;
+			//			}
+
 			numberValues.add(binaryToNumber(binary.substring(i,i+9)));
 		}
 	}
-	
+
 	public static int binaryToNumber(String a) //found online
 	{
 		int ans = 0;
@@ -122,9 +141,9 @@ public class RohanDecoder {
 		}
 		return ans;
 	}
-	
-	
-	
+
+
+
 	public String toBinary(int number)
 	{
 		String cur =Integer.toBinaryString(number);
